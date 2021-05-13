83a287e5529f070930562713c87dd84e992979ef
Nuno <nunotnunes@outlook.com>
2021-05-13 15:24:48 +0100

    added token, simple hello message and setup env

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import discord
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+bot = discord.client()
+
+@bot.event
+async def on_message(message):
+    if message.content =="hello":
+        await message.channel.send("hey")
+
+bot.run(DISCORD_TOKEN)
