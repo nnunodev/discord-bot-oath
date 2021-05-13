51633b70b04a44cf57e00e9cc29fed9f3d4c7788
Nuno <nunotnunes@outlook.com>
2021-05-13 16:33:02 +0100

    added builds command

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
 bot = discord.Client()
+
 
 @bot.event
 async def on_message(message):
-    if message.content =="hello":
-        await message.channel.send("hey")
+    if message.content == "!builds":
+        await message.channel.send("List of builds from Lost Ark discord channel - https://tinyurl.com/tx6rauyn")
 
 bot.run(DISCORD_TOKEN)
