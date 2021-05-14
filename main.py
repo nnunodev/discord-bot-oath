b96a1de3cba9c50dde98e24a3f48d25c5b077a1b
Nuno <nunotnunes@outlook.com>
2021-05-14 08:40:20 +0100

    added howto play command, and changed to bot.command

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import discord
 from discord.ext import commands
 import os
+from discord.ext.commands.core import command
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = discord.Client()
+bot = commands.Bot(command_prefix="!")
+
+# add list of builds
 
 
-@bot.event
-async def on_message(message):
-    if message.content == "!builds":
-        await message.channel.send("List of builds from Lost Ark discord channel - https://tinyurl.com/tx6rauyn")
+@bot.command()
+async def builds(link):
+    await link.channel.send("List of builds from Lost Ark discord channel - https://tinyurl.com/tx6rauyn")
+
+# add how to play guide
+
+
+@bot.command()
+async def howto(link):
+    await link.channel.send("How to play Lost Ark - https://discord.com/channels/842026072773623838/842027776088604692/842341898956963861")
+
+
+# @bot.event
+# async def on_message(message):
+#    if message.content == "!howto":
+#        await message.channel.send("How to play Lost Ark - https://discord.com/channels/842026072773623838/842027776088604692/842341898956963861")
 
 bot.run(DISCORD_TOKEN)
